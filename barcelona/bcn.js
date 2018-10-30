ff9d5aecb9bbb82b5c8697a468147fa090b366d7
Jen Borkowski <jenniferlborkowski@gmail.com>
2018-10-30 16:27:10 -0400

    Add basic BCN page

---
+++ barcelona/bcn.js
@@ -0,0 +1,309 @@
+var bcn = {
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 9,
+        "name": "Parc de la Ciutadella",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.186126,
+          41.388482
+        ],
+        "type": "Point"
+      },
+      "id": "013ff0bdbc7687c9b45e0ed677585eae"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 18,
+        "name": "Montjuic Cable Car ends here?",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.166199,
+          41.364769
+        ],
+        "type": "Point"
+      },
+      "id": "31ca38ce9aed348bfd61de7feff7cfd7"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 19,
+        "name": "Montjuic Castle",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.165941,
+          41.363553
+        ],
+        "type": "Point"
+      },
+      "id": "3a8bbda4198bae48207b937da1923f00"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 7,
+        "name": "MUHBA Placa del Rei",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.177533,
+          41.384127
+        ],
+        "type": "Point"
+      },
+      "id": "5780ef7c98b3c7af59fb784032698484"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bormuth",
+        "id": 2,
+        "type": "eat"
+      },
+      "geometry": {
+        "coordinates": [
+          2.182615,
+          41.385881
+        ],
+        "type": "Point"
+      },
+      "id": "625115d91658e65e13bde4154438ca34"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 4,
+        "name": "Sagrada Familia",
+        "type": "landmark"
+      },
+      "geometry": {
+        "coordinates": [
+          2.174546,
+          41.403499
+        ],
+        "type": "Point"
+      },
+      "id": "6258a083ff945be29acbc97c834a23cd"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 6,
+        "name": "Donkey Tours starting point",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.175121,
+          41.384278
+        ],
+        "type": "Point"
+      },
+      "id": "69660abeaf436efb0d4706d6eefc4072"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Born Center for Culture and Memory",
+        "id": 1,
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.183594,
+          41.385692
+        ],
+        "type": "Point"
+      },
+      "id": "69f0c3281eafdee38a6b0389c2374c73"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 10,
+        "name": "Port Cable Car",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.187861,
+          41.373195
+        ],
+        "type": "Point"
+      },
+      "id": "93140bb75e30052ac3b66af6c5dedb55"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 13,
+        "name": "Les Dues Sicilies - Carrer dels Carders",
+        "type": "eat"
+      },
+      "geometry": {
+        "coordinates": [
+          2.18028,
+          41.386615
+        ],
+        "type": "Point"
+      },
+      "id": "93250ca24fb1bf2d43650fdcc75bb043"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 17,
+        "name": "Montjuic Cable Car starts here?",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.162937,
+          41.368827
+        ],
+        "type": "Point"
+      },
+      "id": "970a3483507078206849458252152af2"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 15,
+        "name": "Plaza with Mediterranean view",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.190103,
+          41.367861
+        ],
+        "type": "Point"
+      },
+      "id": "a80800ca392ac97bf0046788386bccb2"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 8,
+        "name": "Basilica de Santa Maria del Mar",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.181818,
+          41.383518
+        ],
+        "type": "Point"
+      },
+      "id": "b37c6bba7aa3659a7d975019e7a21f2c"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 16,
+        "name": "Port Cable Car ends here",
+        "type": "do"
+      },
+      "geometry": {
+        "coordinates": [
+          2.172347,
+          41.371009
+        ],
+        "type": "Point"
+      },
+      "id": "c2674c011c1f531924516de81f0cc924"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 5,
+        "name": "Cathedral",
+        "type": "landmark"
+      },
+      "geometry": {
+        "coordinates": [
+          2.176268,
+          41.384218
+        ],
+        "type": "Point"
+      },
+      "id": "c87ab9021652700ab0bf8ef5a2300838"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 12,
+        "name": "La Fabrica",
+        "type": "eat"
+      },
+      "geometry": {
+        "coordinates": [
+          2.179471,
+          41.385068
+        ],
+        "type": "Point"
+      },
+      "id": "da3799e760b3d88d924d057f286f788a"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 11,
+        "name": "Al Sur - Carrer dels Carders",
+        "type": "eat"
+      },
+      "geometry": {
+        "coordinates": [
+          2.179928,
+          41.386537
+        ],
+        "type": "Point"
+      },
+      "id": "daedd3a2bc317745f1973dfc5606ec38"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 14,
+        "name": "Santa Caterina Market",
+        "type": "eat"
+      },
+      "geometry": {
+        "coordinates": [
+          2.178162,
+          41.386313
+        ],
+        "type": "Point"
+      },
+      "id": "eb611b20ee7aee3d0691f5ca2585615d"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 3,
+        "name": "Columbus statue",
+        "type": "landmark"
+      },
+      "geometry": {
+        "coordinates": [
+          2.177781,
+          41.37579
+        ],
+        "type": "Point"
+      },
+      "id": "f11804092fc5070e19e7ea924fa65bb5"
+    }
+  ],
+  "type": "FeatureCollection"
+};

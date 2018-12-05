def6ad34c3d145b74f876643bc74bdf9838fd98c
Jen Borkowski <jenniferlborkowski@gmail.com>
2018-12-04 21:06:21 -0500

    Add map for NYC Christmas markets trip

---
+++ nycxmas/nycxmas.js
@@ -0,0 +1,116 @@
+var nycxmas = {
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 5,
+        "name": "Handmade Cavalcade",
+        "location": "Chelsea Market",
+        "description": "Handmade Cavalcade is a pop-up market in the Chelsea Market Event space from Dec 3-9.  Organized by NY Handmade Collective with support from Etsy.",
+        "url": "http://www.handmadecavalcade.com/about/",
+        "locnotes": "Between Ninth and Tenth Aves between W 15th and W 16th St"
+      },
+      "geometry": {
+        "coordinates": [
+          -74.006366,
+          40.742702
+        ],
+        "type": "Point"
+      },
+      "id": "178bf754c430605530a793a93b2fe6de"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1,
+        "name": "Winter Village at Bryant Park",
+        "location": "Bryant Park",
+        "description": "Market by Urbanspace, plus ice skating and stuff.",
+        "url": "https://www.holidayshopsatbryantpark.com/",
+        "locnotes": "Sixth Ave between W 40th and W 42nd"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.983696,
+          40.754286
+        ],
+        "type": "Point"
+      },
+      "id": "4c14150a938831058bb22a7bc03ab0c2"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 6,
+        "name": "Grand Central Holiday Fair",
+        "location": "Grand Central Terminal",
+        "description": "A market of 40 vendors in Vanderbilt Hall.",
+        "url": "https://www.grandcentralterminal.com/event/grand-central-holiday-fair/",
+        "locnotes": "E 42nd at Park (between Madison and Lexington)"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.977669,
+          40.752675
+        ],
+        "type": "Point"
+      },
+      "id": "5fe917a217803f5740d7994b8a14f57c"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 3,
+        "name": "Union Square Holiday Market",
+        "location": "Union Square Park",
+        "description": "An Urbanspace holiday market on the south side of Union Square Park.",
+        "url": "https://www.urbanspacenyc.com/union-square-holiday-market/",
+        "locnotes": "E 14th St between Broadway (east of 5th) and Park"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.990806,
+          40.735918
+        ],
+        "type": "Point"
+      },
+      "id": "a7f5f2070c330f0dbe5d9df42e31602b"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 4,
+        "name": "Columbus Circle Holiday Market",
+        "location": "Columbus Circle",
+        "description": "An Urbanspace market at the SW corner of Central Park.",
+        "url": "https://www.urbanspacenyc.com/columbus-circle-holiday-market",
+        "locnotes": "Eighth Ave and 59th St"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.981953,
+          40.76809
+        ],
+        "type": "Point"
+      },
+      "id": "c0da1ff65d9000ecf259f2bddf622732"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 2,
+        "name": "Rockefeller Center",
+        "locnotes": "Off Fifth Avenue between W 49th and W 50th"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.978613,
+          40.75874
+        ],
+        "type": "Point"
+      },
+      "id": "f9183ed7c9ff284174909ae3df2abe81"
+    }
+  ],
+  "type": "FeatureCollection"
+};

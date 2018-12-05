ec9c50cf883c307bc499edc6e1d50dc64e6251b7
Jen Borkowski <jenniferlborkowski@gmail.com>
2018-12-04 22:24:33 -0500

    Add some bars and restaurants

--- nycxmas/nycxmas.js
+++ nycxmas/nycxmas.js
@@ -3,12 +3,33 @@ var nycxmas = {
     {
       "type": "Feature",
       "properties": {
+        "id": 10,
+        "name": "PDT (Please Don't Tell)",
+        "location": "113 St. Mark's Place",
+        "locnotes": "St. Mark's between First Avenue and Avenue A",
+        "description": "Enter through a phone booth in Crif Dogs. Opens at 6pm.",
+        "url": "https://www.yelp.com/biz/please-dont-tell-new-york-2",
+        "type": "bar"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.983809,
+          40.727081
+        ],
+        "type": "Point"
+      },
+      "id": "11889a2659c13a8fdd9a220f86e18a65"
+    },
+    {
+      "type": "Feature",
+      "properties": {
         "id": 5,
         "name": "Handmade Cavalcade",
         "location": "Chelsea Market",
         "description": "Handmade Cavalcade is a pop-up market in the Chelsea Market Event space from Dec 3-9.  Organized by NY Handmade Collective with support from Etsy.",
         "url": "http://www.handmadecavalcade.com/about/",
-        "locnotes": "Between Ninth and Tenth Aves between W 15th and W 16th St"
+        "locnotes": "Between Ninth and Tenth Aves between W 15th and W 16th St",
+        "type": "market"
       },
       "geometry": {
         "coordinates": [
@@ -27,7 +48,8 @@ var nycxmas = {
         "location": "Bryant Park",
         "description": "Market by Urbanspace, plus ice skating and stuff.",
         "url": "https://www.holidayshopsatbryantpark.com/",
-        "locnotes": "Sixth Ave between W 40th and W 42nd"
+        "locnotes": "Sixth Ave between W 40th and W 42nd",
+        "type": "market"
       },
       "geometry": {
         "coordinates": [
@@ -46,7 +68,8 @@ var nycxmas = {
         "location": "Grand Central Terminal",
         "description": "A market of 40 vendors in Vanderbilt Hall.",
         "url": "https://www.grandcentralterminal.com/event/grand-central-holiday-fair/",
-        "locnotes": "E 42nd at Park (between Madison and Lexington)"
+        "locnotes": "E 42nd at Park (between Madison and Lexington)",
+        "type": "market"
       },
       "geometry": {
         "coordinates": [
@@ -60,17 +83,97 @@ var nycxmas = {
     {
       "type": "Feature",
       "properties": {
+        "id": 7,
+        "name": "Urbanspace @ Vanderbilt",
+        "locnotes": "E 45th St at Vanderbilt Ave",
+        "description": "A food hall with ~20 vendors. Just north of Grand Central.",
+        "url": "https://www.urbanspacenyc.com/urbanspace-vanderbilt/",
+        "type": "food"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.976654,
+          40.754527
+        ],
+        "type": "Point"
+      },
+      "id": "64ecf5dc3d6dc5db5ab79a427814a779"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 8,
+        "name": "Urbanspace @ 570 Lex",
+        "location": "General Electric Building",
+        "locnotes": "570 Lexington Avenue  // at E 51st St, between Park and Third Ave",
+        "description": "A food hall with ~16 vendors.",
+        "url": "https://www.urbanspacenyc.com/570-lexington/",
+        "type": "food"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.972208,
+          40.757004
+        ],
+        "type": "Point"
+      },
+      "id": "792a89394c49466985740235fa8a9849"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 11,
+        "name": "Employees Only",
+        "location": "510 Hudson St",
+        "locnotes": "Hudson St between Christopher St and W 10th St",
+        "description": "Pricey cocktails. Opens at 6. Close to Christopher St PATH station.",
+        "url": "http://employeesonlynyc.com/",
+        "type": "bar"
+      },
+      "geometry": {
+        "coordinates": [
+          -74.006152,
+          40.733445
+        ],
+        "type": "Point"
+      },
+      "id": "7df824719c15ecd90b7db51f78a69f2f"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 9,
+        "name": "Eataly",
+        "location": "200 Fifth Avenue",
+        "locnotes": "Fifth betwen W 23rd and W 24th",
+        "description": "Serra Alpina on the rooftop, and other food options.",
+        "url": "https://www.eataly.com/us_en/stores/nyc-flatiron/",
+        "type": "food"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.9895,
+          40.741961
+        ],
+        "type": "Point"
+      },
+      "id": "9a426a2c6220ca695b3fa15083d7a681"
+    },
+    {
+      "type": "Feature",
+      "properties": {
         "id": 3,
         "name": "Union Square Holiday Market",
         "location": "Union Square Park",
         "description": "An Urbanspace holiday market on the south side of Union Square Park.",
         "url": "https://www.urbanspacenyc.com/union-square-holiday-market/",
-        "locnotes": "E 14th St between Broadway (east of 5th) and Park"
+        "locnotes": "E 14th St between Broadway (east of 5th) and Park",
+        "type": "market"
       },
       "geometry": {
         "coordinates": [
-          -73.990806,
-          40.735918
+          -73.990977,
+          40.735121
         ],
         "type": "Point"
       },
@@ -84,7 +187,8 @@ var nycxmas = {
         "location": "Columbus Circle",
         "description": "An Urbanspace market at the SW corner of Central Park.",
         "url": "https://www.urbanspacenyc.com/columbus-circle-holiday-market",
-        "locnotes": "Eighth Ave and 59th St"
+        "locnotes": "Eighth Ave and 59th St",
+        "type": "market"
       },
       "geometry": {
         "coordinates": [
@@ -98,9 +202,30 @@ var nycxmas = {
     {
       "type": "Feature",
       "properties": {
+        "id": 12,
+        "name": "Proletariat",
+        "location": "102 St. Mark's Pl",
+        "locnotes": "St. Mark's between First Ave and Avenue A",
+        "description": "Small bar with unusual craft beers.",
+        "url": "https://www.proletariatny.com/home-1",
+        "type": "bar"
+      },
+      "geometry": {
+        "coordinates": [
+          -73.984618,
+          40.727169
+        ],
+        "type": "Point"
+      },
+      "id": "d2a355b897a6f49ef45572921423f21c"
+    },
+    {
+      "type": "Feature",
+      "properties": {
         "id": 2,
         "name": "Rockefeller Center",
-        "locnotes": "Off Fifth Avenue between W 49th and W 50th"
+        "locnotes": "Off Fifth Avenue between W 49th and W 50th",
+        "type": "other"
       },
       "geometry": {
         "coordinates": [
